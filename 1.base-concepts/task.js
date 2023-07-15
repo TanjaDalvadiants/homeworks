6b4be391d04a3921b142ed5c7b6fa7b3b6876231
tatiana dalvadiants <eparis@atlassian.com>
2023-07-15 19:09:53 +0400

    Done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,28 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+let solveEquation = (a, b, c) => {
+  if (a == 0)
+    return false;
+  let res = {};
+  let D = b * b - 4 * a * c;
+  //console.log('D = ' + D);
+  if (D < 0)
+    return false;
+  res = D;
+  if (D == 0)
+    res = (-b + Math.sqrt(D)) / (2 * a);
+  else if (D > 0) {
+    let tmp = [];
+    tmp.push((-b + Math.sqrt(D)) / (2 * a));
+    tmp.push((-b - Math.sqrt(D)) / (2 * a));
+    res = tmp;
+  }
+  return res;
 }
+console.log(solveEquation(1,5,4));
+console.log(solveEquation(1, 2, 1));
+console.log(solveEquation(1, 2, 10));
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
