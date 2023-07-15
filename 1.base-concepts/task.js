6d8e76e534722cd1d87b15040cee14c5302d7884
tatiana dalvadiants <eparis@atlassian.com>
2023-07-15 21:37:51 +0400

    Done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,22 +1,20 @@
 "use strict"
-let solveEquation = (a, b, c) => {
-  if (a == 0)
-    return false;
-  let res = {};
-  let D = b * b - 4 * a * c;
-  //console.log('D = ' + D);
-  if (D < 0)
-    return false;
-  res = D;
-  if (D == 0)
-    res = (-b + Math.sqrt(D)) / (2 * a);
-  else if (D > 0) {
-    let tmp = [];
-    tmp.push((-b + Math.sqrt(D)) / (2 * a));
-    tmp.push((-b - Math.sqrt(D)) / (2 * a));
-    res = tmp;
-  }
-  return res;
+
+function solveEquation(a, b, c) {
+	if (a === 0) return false;
+
+	let arr = [];
+	let d = Math.pow(b, 2) - 4 * a * c
+	if (d < 0) return arr;
+	if (d === 0) {
+		arr =[ -b / (2 * a)]
+	} else if (d > 0) {
+		let tmp = []
+		tmp.push((-b + Math.sqrt(d)) / (2 * a));
+		tmp.push((-b - Math.sqrt(d)) / (2 * a));
+		arr = tmp;
+	}
+	return arr;
 }
 console.log(solveEquation(1,5,4));
 console.log(solveEquation(1, 2, 1));
@@ -25,5 +23,14 @@ console.log(solveEquation(1, 2, 10));
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let percentHundredths = percent / 100;
+	let monthPercent = percentHundredths / 12;
+	let creditBody = amount - contribution;
+
+	// Платёж = S * (P + (P / (((1 + P)^n) - 1)))
+	let payment = creditBody * (monthPercent + (monthPercent / (Math.pow(1 + monthPercent, countMonths) - 1)));
+
+	let sumPay = +(payment * countMonths).toFixed(2);
+
+	return sumPay
 }
