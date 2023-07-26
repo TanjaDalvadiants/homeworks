008a8420513bba32a6559553a7599d048d542833
tatiana dalvadiants <eparis@atlassian.com>
2023-07-26 11:58:13 +0400

    Done

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -99,3 +99,35 @@ class Library {
     }
 
 }
+
+class Student {
+    constructor (name){
+        this.name = name;
+        this.marks = {};
+    }
+    addMark(mark, subject){
+        if (mark < 2 || mark >5){
+            return;
+        }
+        if (this.marks.hasOwnProperty(subject)){
+            this.marks[subject] = [];
+        }
+        this.marks[subject].push(mark);
+    }
+
+    getAverageBySubject(subject){
+        if (this.marks.hasOwnProperty(subject)){
+            return this.marks[subject].reduce((acc, mark) => acc + mark / this.marks[subject].length, 0);
+        }
+        return 0;
+    }
+
+    getAverage(){
+        const subjects = Object.keys(this.marks);
+        if (subjects.length === 0){
+            return 0;
+        }
+        return subjects.reduce((acc, subject) => acc + this.getAverageBySubject(subject), 0)/ subjects.length; 
+    }
+
+}
